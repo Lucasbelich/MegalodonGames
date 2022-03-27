90cfe13b5af08792255603ae1cc6ca04c5457875
Lucasbelich <lucasbelich1@gmail.com>
2022-03-27 18:53:16 -0300

    NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider, Box, Button, Text, Container } from "@chakra-ui/react";
+import { ChakraProvider, Box, Button, Text,  } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
